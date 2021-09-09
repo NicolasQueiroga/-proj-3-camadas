1d7ab0c319bcee7733f57597168e9a220f12d38e
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-09-09 19:13:05 -0300

    ...

--- src-serv/aplicacao.py
+++ src-serv/aplicacao.py
@@ -25,34 +25,60 @@ class Server:
         self.com1 = enlace(self.serial_number)
 
         # Inicializando atributos de "head"
+        self.msg_type = b''
         self.cli_id = int(42).to_bytes(1, 'big')
-        self.serv_id = int(69).to_bytes(1, 'big')
+        self.serv_id = int(21).to_bytes(1, 'big')
         self.msg_id = b''
-        self.packs_qty = b''
-        self.pack_id = 0
+        self.pkgs_qty = b''
+        self.pkg_id = b''
         self.payload_size = b''
 
+        # Inicializando atributo de EOP
+        self.EOP = b'\x4C\x4F\x56\x55'
+
         # Inicializando atributos de apoio
-        self.FLAGS = {'ENABLED': False}
+        self.rx_buffer = b''
+        self.head_size = 10
+        self.head = b''
+        self.payload = b''
+        self.TIPO_MENSAGEM = {'handshake': b'\x01',
+                              'handshake-response': b'\x02',
+                              'data': b'\x03',
+                              'data-ok': b'\x04',
+                              'timeout': b'\x05',
+                              'error': b'\x06'}
+        self.FLAGS = {'ENABLED': False,
+                      'ERROR': False,
+                      'GOT_PKG': False,
+                      'GOT_HEAD': False,
+                      'GOT_PKG_INFO': False,
+                      'GOT_EOP': False}
+
+    def status(self, step):
+        msgs = ['A comunicação foi iniciada com sucesso!',
+                'A comunicação não foi ativada, utilize o método "activate_comunication"']
+        print(f'---> {msgs[step]}')
 
     def activate_comunication(self):
         self.com1.enable()
         self.FLAGS['ENABLED'] = True
 
-    def read_payload(self):
+    def read_pkg(self):
         if self.FLAGS['ENABLED']:
-            pass
+            self.rx_buffer = self.com1.getData()[0]
         else:
-            print('---> A comunicação não foi ativada, utilize o método "activate_comunication"')
+            self.status(1)
 
+    def read_head(self):
+        self.head = self.com1.getData(self.head_size)[0]
+        self.msg_type = self.head[0]
 
-serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+    def read_payload(self):
+        pass
 
 
 def main():
     try:
-        com1 = enlace(serialName)
-        com1.enable()
         print('\n---> A comunicação foi aberta com sucesso!')
 
         t1 = timeit.default_timer()
