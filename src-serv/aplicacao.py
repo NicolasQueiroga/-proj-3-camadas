1105d3183a8254226707d350eac5d942f3407fea
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-09-15 23:37:07 -0300

    finalizando maior parte

--- src-serv/aplicacao.py
+++ src-serv/aplicacao.py
@@ -72,22 +72,33 @@ class Server:
                        'read_pkg': '',
                        'read_head_ok': '', 'read_head_!ok': '',
                        'got_hs': '', 'sent_hs': '', 'got_data': '', 'sent_dataOK': '', 'check_head_!ok': '',
-                       '': ''}
+                       'got_pl': '', 'got_filetype': '', 'read_payload_!ok': '',
+                       'check_eop_!ok': ''}
 
     def status(self, msg):
         print(f'---> {self.STATUS[msg]}!')
 
     def activate_comunication(self):
         self.com1.enable()
-        self.status(0)
+        self.status('activate_com_ok')
         self.FLAGS['ENABLED'] = True
 
     def disable_comunications(self):
         self.com1.disable()
-        self.status(1)
+        self.status('disable_com')
         self.FLAGS['ENABLED'] = False
+    
+    def init(self):
+        self.FLAGS['GOT_HEAD'] = False
+        self.FLAGS['CHECK_HEAD'] = False
+        self.FLAGS['GOT_PAYLOAD'] = False
+        self.FLAGS['END_OF_MSG'] = False
+
 
     def read_pkg(self):
+        self.init()
+        self.status('read_pkg')
+
         if self.FLAGS['ENABLED']:
             self.read_head()
         else:
@@ -95,62 +106,95 @@ class Server:
 
         if self.FLAGS['GOT_HEAD']:
             self.check_head()
+        else:
+            return 'error'
+
         if self.FLAGS['CHECK_HEAD']:
             self.read_payload()
+        else:
+            return 'error'
+
         if self.FLAGS['GOT_PAYLOAD']:
             self.check_eop()
+        else:
+            return 'error'
+
         if self.FLAGS['END_OF_MSG']:
             self.disable_comunications()
             return 'END'
+        else:
+            self.status('eop_error')
+            return 'error'
+
 
     def read_head(self):
-        self.head = self.com1.getData(self.head_size)[0]
-        for i in arange(self.head_size):
-            self.head_container[i] = self.head[i]
-        self.msg_id_list.append(self.msg_id)
-        self.pkgs_qty_list.append(self.pkgs_qty)
-        self.pkg_id_list.append(self.pkg_id)
-        self.pkg_counter = int.to_bytes(self.pkgs_qty, 'big')
-        self.status(2)
-        self.FLAGS['GOT_HEAD'] = True
+        try:
+            self.head = self.com1.getData(self.head_size)[0]
+            for i in arange(self.head_size):
+                self.head_container[i] = self.head[i]
+
+            # mudar para dicio {pkg_id: {}}
+            self.msg_id_list.append(self.msg_id)
+            self.pkgs_qty_list.append(self.pkgs_qty)
+            self.pkg_id_list.append(self.pkg_id)
+            self.pkg_counter = int.to_bytes(self.pkgs_qty, 'big')
+
+            self.status('read_head_ok')
+            self.FLAGS['GOT_HEAD'] = True
+        except Exception as erro:
+            self.status('read_head_!ok')
 
     def check_head(self):
-        if self.msg_type == b'\x01' and not self.FLAGS['SENT_HS_RESPONSE']:
-            head_response_list = [self.msg_type_dict['handshake-response'], self.cli_id, self.serv_id,
-                                  self.msg_id, self.pkgs_qty, self.pkg_id, self.payload_size, b'\x00\x00\x00']
-            head_response = b''.join(head_response_list)
-            self.status(3)
-            self.com1.sendData(head_response)
-            self.status(5)
-            self.FLAGS['SENT_HS_RESPONSE'] = True
-            self.FLAGS['CHECK_HEAD'] = True
-            self.FLAGS['HS_PAYLOAD'] = True
-        elif self.msg_type == b'\x03' and self.FLAGS['SENT_HS_RESPONSE']:
-            self.status(3)
-        else:
-            
-            pass
+        try:
+            if self.msg_type == b'\x01' and not self.FLAGS['SENT_HS_RESPONSE']:
+                self.status('got_hs')
+                head_response_list = [self.msg_type_dict['handshake-response'], self.cli_id, self.serv_id,
+                                    self.msg_id, self.pkgs_qty, self.pkg_id, self.payload_size, b'\x00\x00\x00']
+                head_response = b''.join(head_response_list)
+                self.com1.sendData(head_response)
+                self.status('sent_hs')
+                self.FLAGS['SENT_HS_RESPONSE'] = True
+                self.FLAGS['CHECK_HEAD'] = True
+                self.FLAGS['HS_PAYLOAD'] = True
+            elif self.msg_type == b'\x03' and self.FLAGS['SENT_HS_RESPONSE']:
+                self.status('got_data')
+                dataOK_response_list = [self.msg_type_dict['data-ok'], self.cli_id, self.serv_id,
+                                    self.msg_id, self.pkgs_qty, self.pkg_id, self.payload_size, b'\x00\x00\x00']
+                dataOK_response = b''.join(dataOK_response_list)
+                self.com1.sendData(dataOK_response)
+                self.status('sent_dataOK')
+                self.FLAGS['CHECK_HEAD'] = True
+        except Exception as erro:
+            self.status('check_head_!ok')
+        
 
     def read_payload(self):
-        if self.FLAGS['HS_PAYLOAD']:
-            self.com1.getData(self.payload_size)[0]
-            self.FLAGS['HS_PAYLOAD'] = False
-        elif int.to_bytes(self.pkg_id, 'big') == 0:
-            self.file_type = self.com1.getData(self.payload_size)[
-                0].decode('ascii')
-            self.FLAGS['GOT_FILE_TYPE'] = True
-        else:
-            self.payload += self.com1.getData(self.payload_size)[0]
-        self.FLAGS['GOT_PAYLOAD'] = True
-        self.status(6)
+        try:
+            if self.FLAGS['HS_PAYLOAD']:
+                self.com1.getData(self.payload_size)[0]
+                self.FLAGS['HS_PAYLOAD'] = False
+            elif int.to_bytes(self.pkg_id, 'big') == 0 and not self.FLAGS['GOT_FILE_TYPE']:
+                self.file_type = self.com1.getData(self.payload_size)[
+                    0].decode('ascii')
+                self.FLAGS['GOT_FILE_TYPE'] = True
+                self.status('got_filetype')
+            else:
+                self.payload += self.com1.getData(self.payload_size)[0]
+            self.FLAGS['GOT_PAYLOAD'] = True
+            self.status('got_pl')
+        except Exception as erro:
+            self.status('read_payload_!ok')
 
     def check_eop(self):
-        self.eop = self.com1.getData(self.eop_size)
-        self.FLAGS['GOT_EOP'] = True
-        if self.eop == self.EOP:
-            self.pkg_counter -= 1
-            if self.pkg_counter == 0:
-                self.FLAGS['END_OF_MSG'] = True
+        try:
+            self.eop = self.com1.getData(self.eop_size)
+            self.FLAGS['GOT_EOP'] = True
+            if self.eop == self.EOP:
+                self.pkg_counter -= 1
+                if self.pkg_counter == 0:
+                    self.FLAGS['END_OF_MSG'] = True
+        except Exception as erro:
+            self.status('check_eop_!ok')
 
 
 def main():
@@ -160,6 +204,9 @@ def main():
         server.activate_comunication()
         while (ans != 'END' or ans != 'error'):
             ans = server.read_pkg()
+        
+        if ans == 'error':
+            pass
 
     except Exception as erro:
         print("ops! :-\\")
